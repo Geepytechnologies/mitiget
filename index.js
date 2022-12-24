9a9635fd75283eb169623466968242fcd2246d62
geepy technologies <geepytechnologies@gmail.com>
2022-12-24 14:35:03 +0100

    fresh

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Credentials", true)
   next();
 })
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
+app.use(cors({origin: process.env.DOMAIN, credentials: true}));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoute);
