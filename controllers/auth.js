2b581c74f9376add6a6174b0143d0e99ddc342d7
geepy technologies <geepytechnologies@gmail.com>
2023-02-11 11:28:12 +0100

    new

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,73 +5,73 @@ import { createError } from "../error.js";
 import jwt from "jsonwebtoken";
 
 
-export const signup = async (req,res,next)=>{
-  const existingusername = await User.findOne({username: req.body.username})
-  const existingemail = await User.findOne({email: req.body.email})
-  const existinguser = await User.findOne({email: req.body.email, username: req.body.username})
-  try{
-    if(existinguser) return next(createError(409, "User already exists"))
-    else if(existingemail) return next(createError(409, "Email already exists"))
-    else if(existingusername) return next(createError(409, "Username is unavailable"))
-    else{
-    const salt = bcrypt.genSaltSync(10);
-    const hashedpassword = bcrypt.hashSync(req.body.password, salt);
-    const user = new User({...req.body, password: hashedpassword});
-    await user.save();
-    res.status(201).json("User has been Created");
+export const signup = async (req, res, next) => {
+  const existingusername = await User.findOne({ username: req.body.username })
+  const existingemail = await User.findOne({ email: req.body.email })
+  const existinguser = await User.findOne({ email: req.body.email, username: req.body.username })
+  try {
+    if (existinguser) return next(createError(409, "User already exists"))
+    else if (existingemail) return next(createError(409, "Email already exists"))
+    else if (existingusername) return next(createError(409, "Username is unavailable"))
+    else {
+      const salt = bcrypt.genSaltSync(10);
+      const hashedpassword = bcrypt.hashSync(req.body.password, salt);
+      const user = new User({ ...req.body, password: hashedpassword });
+      await user.save();
+      res.status(201).json("User has been Created");
     }
-  }catch(err){
+  } catch (err) {
     next(err);
   }
 }
-export const signin = async (req,res,next)=>{
-  try{
-    const user = await User.findOne({username: req.body.username})
-    if(!user) return next(createError(404, "User not found"));
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) return next(createError(404, "User not found"));
 
 
     const isMatched = bcrypt.compareSync(req.body.password, user.password);
-    if(!isMatched) return next(createError(400,"wrong credentials"));
+    if (!isMatched) return next(createError(400, "wrong credentials"));
 
-    const token = jwt.sign({id: user._id}, process.env.SECRET)
-    const {password, ...others} = user._doc;
+    const token = jwt.sign({ id: user._id }, process.env.SECRET)
+    const { password, ...others } = user._doc;
 
     res.cookie('_token', token, {
-        httpOnly: true,
-        // secure: true,
-        // maxAge: 3600 * 24,
+      httpOnly: true,
+      // secure: true,
+      // maxAge: 3600 * 24,
     })
-    .status(200).json(others);
-  }catch(err){
+      .status(200).json(others);
+  } catch (err) {
     next(err);
   }
 }
 
 export const googleAuth = async (req, res, next) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
-    if (user) {
+    const user = await User.findOne({ username: req.body.username });
+    // if (user) {
       const token = jwt.sign({ id: user._id }, process.env.SECRET);
-      res
-        .cookie("_token", token, {
-          httpOnly: true,
-        })
-        .status(200)
-        .json(user._doc);
-    } else {
-      const newUser = new User({
-        ...req.body,
-        fromGoogle: true,
-      });
-      const savedUser = await newUser.save();
-      const token = jwt.sign({ id: savedUser._id }, process.env.SECRET);
-      res
-        .cookie("_token", token, {
-          httpOnly: true,
-        })
-        .status(200)
-        .json(savedUser._doc);
-    }
+
+      res.cookie("_token", token, {
+        // httpOnly: true,
+      })
+      .status(200)
+      .json(user._doc);
+    // } else {
+    //   const newUser = new User({
+    //     ...req.body,
+    //     fromGoogle: true,
+    //   });
+    //   const savedUser = await newUser.save();
+    //   const token = jwt.sign({ id: savedUser._id }, process.env.SECRET);
+
+    //   // res.cookie("_token", token, {
+    //   //   httpOnly: true,
+    //   // })
+    //   res.status(200)
+    //     .json(savedUser._doc);
+    // }
   } catch (err) {
     next(err);
   }
--- index.js
+++ index.js
@@ -17,11 +17,13 @@ dotenv.config();
 
 const myclient = path.join(__dirname + "../frontend/dist"); //
 
+app.use(cors({origin: process.env.DOMAIN, credentials: true}));
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Origin", process.env.DOMAIN);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next();
 })
-app.use(cors({origin: process.env.DOMAIN, credentials: true}));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoute);
