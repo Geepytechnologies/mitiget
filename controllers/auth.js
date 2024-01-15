092bc74fb0f756b9a18bd08947f8215f3d8c7bb4
geepytechnologies <geepytechnologies@gmail.com>
2024-01-15 08:45:43 +0100

    changed signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,7 +22,7 @@ export const signup = async (req, res, next) => {
 };
 export const signin = async (req, res, next) => {
   try {
-    const user = await User.findOne({ username: req.body.email });
+    const user = await User.findOne({ email: req.body.email });
     if (!user) return next(createError(404, "User not found"));
 
     const isMatched = bcrypt.compareSync(req.body.password, user.password);
